4be83285a0bf0553a54e6ead75b01750a908c017
Ksenia2781 <128595271+Ksenia2781@users.noreply.github.com>
2023-05-03 21:46:13 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5507394522:AAH24lVBG1HgWsLrRCnaGP0MIWmHvUj3tG4"
+
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'фунт': 'GBP',
+    'лира': 'TRY',
+    'тенге': 'KZT',
+}
